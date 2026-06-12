8da071731bf5a349206d734c42d705574b5d1d43
Akira28hk <118812660+Akira28hk@users.noreply.github.com>
2026-06-12 21:43:23 +0800

    Lock Pan

--- firmware/src/controllers/MotionController.cpp
+++ firmware/src/controllers/MotionController.cpp
@@ -121,8 +121,13 @@ void MotionController::compute(const float raw[9], const float* baseline, float 
 
   // Apply sign fixes and gains
   float y[6];
-  y[AXIS_TX] = Config::SIGN_AXIS[AXIS_TX] * tx * Config::GAIN_T[AXIS_TX];
-  y[AXIS_TY] = Config::SIGN_AXIS[AXIS_TY] * ty * Config::GAIN_T[AXIS_TY];
+  if (Config::LOCKPAN) {
+    y[AXIS_TX] = 0;
+    y[AXIS_TY] = 0;
+  } else {
+    y[AXIS_TX] = Config::SIGN_AXIS[AXIS_TX] * tx * Config::GAIN_T[AXIS_TX];
+    y[AXIS_TY] = Config::SIGN_AXIS[AXIS_TY] * ty * Config::GAIN_T[AXIS_TY];    
+  }
   y[AXIS_TZ] = Config::SIGN_AXIS[AXIS_TZ] * tz * Config::GAIN_T[AXIS_TZ];
   y[AXIS_RX] = Config::SIGN_AXIS[AXIS_RX] * rx * Config::GAIN_R[AXIS_RX - 3];
   y[AXIS_RY] = Config::SIGN_AXIS[AXIS_RY] * ry * Config::GAIN_R[AXIS_RY - 3];
