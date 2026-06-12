aa798545a538d151d6673fc525fb8633dd5072bb
Akira28hk <118812660+Akira28hk@users.noreply.github.com>
2026-06-12 20:39:59 +0800

    Wake on sensor (#4)
    * wake on motion
    
    * wake on motion
    
    * wake on motion
    
    * Update SleepState.cpp
    
    * wake on motion
    
    * wake on motion
    
    * wake

--- firmware/include/controllers/HIDController.h
+++ firmware/include/controllers/HIDController.h
@@ -9,6 +9,11 @@ class HIDController {
   void task();
   bool sendReports(const float motion[6], uint16_t buttonBits);
 
+  // Convenience for state-transition flushes: sends zero motion with
+  // the supplied button bits so the host stops extrapolating whatever
+  // the previous tick reported.
+  bool sendNeutral(uint16_t buttonBits);
+
  private:
   struct __attribute__((packed)) ReportAxes {
     int16_t x, y, z, rx, ry, rz;
